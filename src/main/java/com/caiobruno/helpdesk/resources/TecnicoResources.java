0eaac140ace75886024259fbae78401d946f19e5
Caio Bruno <caiobruno90@yahoo.com>
2023-10-26 15:08:43 -0300

    Findall para tecnicos

--- src/main/java/com/caiobruno/helpdesk/resources/TecnicoResources.java
+++ src/main/java/com/caiobruno/helpdesk/resources/TecnicoResources.java
@@ -1,5 +1,8 @@
 package com.caiobruno.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.caiobruno.helpdesk.domain.Tecnico;
 import com.caiobruno.helpdesk.domain.dtos.TecnicoDto;
 import com.caiobruno.helpdesk.services.TecnicoServices;
+
+
 
 @RestController
 @RequestMapping(value = "/tecnicos")
@@ -22,5 +27,13 @@ public class TecnicoResources {
 	public ResponseEntity<TecnicoDto> findById(@PathVariable Integer id){
 		Tecnico obj =services.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDto(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDto>> findAll(){
+		List<Tecnico> list = services.findAll();
+		List<TecnicoDto> listDTO = list.stream().map(obj -> new TecnicoDto(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/caiobruno/helpdesk/services/TecnicoServices.java
+++ src/main/java/com/caiobruno/helpdesk/services/TecnicoServices.java
@@ -1,5 +1,6 @@
 package com.caiobruno.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoServices {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado id : " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 }
