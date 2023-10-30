d4e5b9b508ee5e496915f5486132f78f2036177d
Caio Bruno <caiobruno90@yahoo.com>
2023-10-30 16:39:54 -0300

    Update para chamado

--- src/main/java/com/caiobruno/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/caiobruno/helpdesk/resources/ChamadoResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,25 +27,33 @@ public class ChamadoResource {
 
 	@Autowired
 	private ChamadoService service;
-	
-	@GetMapping(value =  "/{id}")
-	public ResponseEntity<ChamadoDto> findBuId(@PathVariable Integer id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDto> findBuId(@PathVariable Integer id) {
 		Chamado obj = service.findById(id);
-		return ResponseEntity.ok().body(new ChamadoDto(obj)); 
-		
+		return ResponseEntity.ok().body(new ChamadoDto(obj));
+
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<ChamadoDto>> findAll(){
+	public ResponseEntity<List<ChamadoDto>> findAll() {
 		List<Chamado> list = service.findById();
-		List<ChamadoDto> listDTO= list.stream().map(obj-> new ChamadoDto(obj)).collect(Collectors.toList());
+		List<ChamadoDto> listDTO = list.stream().map(obj -> new ChamadoDto(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<ChamadoDto> create(@Valid @RequestBody ChamadoDto objDto){
+	public ResponseEntity<ChamadoDto> create(@Valid @RequestBody ChamadoDto objDto) {
 		Chamado obj = service.create(objDto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDto> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDto objDto) {
+		Chamado newObj = service.update(id, objDto);
+
+		return ResponseEntity.ok().body(new ChamadoDto(newObj));
+
 	}
 }
--- src/main/java/com/caiobruno/helpdesk/services/ChamadoService.java
+++ src/main/java/com/caiobruno/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.caiobruno.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,20 +21,19 @@ import jakarta.validation.Valid;
 @Service
 public class ChamadoService {
 
-	
 	@Autowired
 	private ChamadoRepository repository;
-	
+
 	@Autowired
 	private TecnicoServices tecnicoServices;
-	
+
 	@Autowired
 	private ClienteServices clienteServices;
-	
+
 	public Chamado findById(Integer id) {
-		Optional<Chamado> obj= repository.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado ID :"+ id));
-		
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ID :" + id));
+
 	}
 
 	public List<Chamado> findById() {
@@ -43,22 +43,36 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDto objDto) {
 		return repository.save(newChamado(objDto));
 	}
-	
+
+	public Chamado update(Integer id, @Valid ChamadoDto objDto) {
+		objDto.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDto);
+
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDto obj) {
 		Tecnico tecnico = tecnicoServices.findById(obj.getTecnico());
 		Cliente cliente = clienteServices.findById(obj.getCliente());
-		
+
 		Chamado chamado = new Chamado();
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if (obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
 		chamado.setStatus(Status.toEnum(obj.getStatus()));
-	    chamado.setTitulo(obj.getTitulo());
-	    chamado.setObservacoe(obj.getObservacoe());
-	    
-	    return chamado;
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoe(obj.getObservacoe());
+
+		return chamado;
 	}
+
 }
